513e18fe433412d540e16d3c6aa1f9f0cff7d6f5
Sami Samhuri <sami.samhuri@gmail.com>
2010-11-26 21:14:37 -0800

    use a template for projects

--- proj/store.js
+++ proj/store.js
@@ -1,22 +1,33 @@
 (function() {
   if (typeof localStorage === 'undefined') return
   var global = this
-  global.ObjectStore = {
-    clear: function() {
-      localStorage.clear()
-    },
-    get: function(key) {
-      var val = localStorage.getItem(key)
-      return typeof val === 'string' ? JSON.parse(val) : val
-    },
-    key: function(n) {
-      return localStorage.key(n)
-    },
-    set: function(key, val) {
-      localStorage.setItem(key, JSON.stringify(val))
-    },
-    remove: function(key) {
-      localStorage.remove(key)
+  global.createObjectStore = function(namespace) {
+    function makeKey(k) {
+      return '--' + namespace + '-' + (k || '')
+    }
+    return {
+      clear: function() {
+        var i = localStorage.length
+          , k
+          , prefix = new RegExp('^' + makeKey())
+        while (--i) {
+          k = localStorage.key(i)
+          if (k.match(prefix)) {
+            localStorage.remove(k)
+          }
+        }
+      },
+      get: function(key) {
+        var val = localStorage.getItem(makeKey(key))
+        return typeof val === 'string' ? JSON.parse(val) : val
+      },
+      set: function(key, val) {
+        localStorage.setItem(makeKey(key), JSON.stringify(val))
+      },
+      remove: function(key) {
+        localStorage.removeItem(makeKey(key))
+      }
     }
   }
+  global.ObjectStore = global.createObjectStore('default')
 }())
