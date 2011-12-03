9df136d176294e1995d518430ee2fdc8e5b5d60c
Sami Samhuri <sami.samhuri@gmail.com>
2011-12-03 14:28:59 -0800

    create public/js and public/css if necessary

--- bin/minify.sh
+++ bin/minify.sh
@@ -2,6 +2,7 @@
 
 setopt extendedglob
 
+[[ ! -d public/js ]] && mkdir public/js
 for js (assets/js/*.js) {
     target=public/js/${${js:t}%.js}.min.js
     if [ ! -f $target ] || [ $js -nt $target ]; then
@@ -10,6 +11,7 @@ for js (assets/js/*.js) {
     fi
 }
 
+[[ ! -d public/css ]] && mkdir public/css
 for css (assets/css/*.css) {
     target=public/css/${${css:t}%.css}.min.css
     if [ ! -f $target ] || [ $css -nt $target ]; then
