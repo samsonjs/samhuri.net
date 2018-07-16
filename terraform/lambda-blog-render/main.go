d9dd720688e97136c78b390a5430790873220f5b
Sami Samhuri <sami@samhuri.net>
2018-07-15 17:22:21 -0700

    add some notes

--- terraform/lambda-blog-render/main.go
+++ terraform/lambda-blog-render/main.go
@@ -3,17 +3,32 @@ package main
 import (
 	"context"
 	"fmt"
-	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/aws/aws-lambda-go/events"
 )
 
-type MyEvent struct {
-	Name string `json:"name"`
-}
+func handler(ctx context.Context, s3Event events.S3Event) {
+	for _, record := range s3Event.Records {
+		s3 := record.S3
+		fmt.Printf("[%s - %s] Bucket = %s, Key = %s \n", record.EventSource, record.EventTime, s3.Bucket.Name, s3.Object.Key)
+		/*
+			Processing rules:
+			- drafts/*.md: render the draft
+			- posts/*.md: render the post, archive, index, and date indexes (yearly, monthly)
+			- templates/*: render everything
+			- public/*: copy to the public bucket
+			- anything else: warning or error?
 
-func HandleRequest(ctx context.Context, name MyEvent) (string, error) {
-	return fmt.Sprintf("Hello %s!", name.Name), nil
+			Public bucket structure:
+			- index.html
+			- feed.xml
+			- feed.json
+			- archive/index.html
+			- posts/yyyy/index.html
+			- posts/yyyy/mm/{slug}.html
+		*/
+	}
 }
 
 func main() {
-	lambda.Start(HandleRequest)
+	lambda.Start(handler)
 }
